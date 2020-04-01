52fa9b36ed23abb1c2c29dd1db7b9e18f463f049
Jake Thomson <jakethomson0@gmail.com>
2020-04-01 15:54:49 +0100

    Added ValidationService

--- src/main/java/com/jakethomson/tictactoe/controllers/MoveController.java
+++ src/main/java/com/jakethomson/tictactoe/controllers/MoveController.java
@@ -1,11 +1,10 @@
 package com.jakethomson.tictactoe.controllers;
 
 import com.jakethomson.tictactoe.models.Game;
+import com.jakethomson.tictactoe.repositories.GameRepository;
 import com.jakethomson.tictactoe.services.MoveService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.jakethomson.tictactoe.services.ValidationService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -15,8 +14,12 @@ public class MoveController {
 
     // Dependency injection.
     private final MoveService moveService;
-    public MoveController(MoveService moveService) {
+    private final GameRepository repository;
+    private final ValidationService validationService;
+    public MoveController(MoveService moveService, GameRepository repository, ValidationService validationService) {
         this.moveService = moveService;
+        this.repository = repository;
+        this.validationService = validationService;
     }
 
     /* When a POST request to /games/{id} is made, the game with the
@@ -24,6 +27,15 @@ public class MoveController {
     @PostMapping("/games/{id}")
     public Game move(@PathVariable long id,
                      @RequestBody Map<String, String[]> body) {
-        return moveService.move(id, body.get("board"));
+        Game game = repository.findById(id);
+        char playerSide;
+        if(game.getPlayer_x_id() == "Server")
+            playerSide = 'O';
+        else
+            playerSide = 'X';
+        if(validationService.validateMoveInput(game.getBoard(), body.get("board"), playerSide))
+            return moveService.move(game, body.get("board"));
+        else
+            return game;
     }
 }
